aa65f538a88c161915fb752dc2c82508b5d20681
isaacoliveira1 <isaac-nicholas@hotmail.com>
2019-08-21 11:20:59 -0300

    Atualização

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define COLUMNS 30
-#define ROWS 10
+#define COLUMNS 60
+#define ROWS 25
 
 void init(char matrix[ROWS][COLUMNS]){
             int i,j;
             for (i=0; i<ROWS; i++){
                 for (j=0; j<COLUMNS; j++){
-                    matrix[i][j] = '-';
+                    matrix[i][j] = 177;
                 }
             }
         }
@@ -27,9 +27,27 @@ void printMatrix (char matrix[ROWS][COLUMNS]){
 
 int main (){
     char matrix[ROWS][COLUMNS];
+    int posi;
+    int posj;
+    int poxf, poxj;
+    posi = ROWS/2;
+    posj = COLUMNS/2;
+   
+ 
+    system("cls");
+     system("COLOR 9E");
     init(matrix);
-    printMatrix(matrix);
+       while (1)
+    {      system("cls");
+    system("COLOR E");
 
+         matrix[posi][posj] =  '@';
+         printMatrix(matrix);
+
+         matrix[posi][posj] =  177;
+         if (posj < COLUMNS) posj ++;
+    
+    
     system("pause");
     return 0;
 }

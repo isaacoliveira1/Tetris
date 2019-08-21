e689263a2151e9c08ade84bc1a6d018bd212a6e0
isaacoliveira1 <isaac-nicholas@hotmail.com>
2019-08-21 09:51:45 -0300

    Atualização

--- login.sh
+++ login.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-git config --global user.name isaacoliveira1
-git config --global user.email isaac-nicholas@hotmail.com
+
+echo "Digite o usuário!"
+read username
+echo "Digite o email"
+read email
+git config --global user.name $username
+git config --global user.email $email
 echo "Logando![...]"; sleep 1;

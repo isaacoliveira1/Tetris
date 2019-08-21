101eacf22f95d87faff88c958dd812ba5a7a8e37
isaacoliveira1 <isaac-nicholas@hotmail.com>
2019-08-21 10:42:19 -0300

    Atualização

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define COLUMNS 30
+#define ROWS 10
+
+void init(char matrix[ROWS][COLUMNS]){
+            int i,j;
+            for (i=0; i<ROWS; i++){
+                for (j=0; j<COLUMNS; j++){
+                    matrix[i][j] = '-';
+                }
+            }
+        }
+void printMatrix (char matrix[ROWS][COLUMNS]){
+              int i,j;
+            for (i=0; i<ROWS; i++){
+                    
+                for (j=0; j<COLUMNS; j++){
+                   printf("%c", matrix[i][j]);
+
+                }
+                printf ("\n");
+            }
+        }
+
 
 
 int main (){
-    printf ("Hello World!");
+    char matrix[ROWS][COLUMNS];
+    init(matrix);
+    printMatrix(matrix);
 
-
-
+    system("pause");
     return 0;
 }

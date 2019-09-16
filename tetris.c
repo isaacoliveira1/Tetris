f5412a190904662522f0021d36147e67952a826b
isaacoliveira1 <50579867+isaacoliveira1@users.noreply.github.com>
2019-09-16 14:25:05 -0300

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -26,6 +26,37 @@
                             }
                     }
         }
+/*
+    Mostra o conteúdo da matriz principal na tela 
+    do computador.
+*/
+void printMatrix(char matrix[ROWS][COLUMNS]){
+    int i,j;
+
+    printf("\t\t\t");
+    //Linha de cima
+    for(j=0; j<COLUMNS+2; j++){
+        printf("*");
+    }
+    printf("\n");
+
+    //Matriz do jogo
+    for(i=0; i<ROWS; i++){        
+        printf("\t\t\t*");
+        for(j=0; j<COLUMNS; j++){
+            printf("%c", matrix[i][j]);
+        }
+        printf("*\n");
+    }
+
+    printf("\t\t\t");
+    //Linha de baixo
+    for(j=0; j<COLUMNS+2; j++){
+        printf("*");
+    }
+    printf("\n");
+
+}
 
 
 

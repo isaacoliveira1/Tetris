50c197c1e296f23c5ab5b131824fca6b8e14ffb8
isaacoliveira1 <isaac-nicholas@hotmail.com>
2019-09-18 11:23:31 -0300

    Movimentação

--- up.sh
+++ up.sh
@@ -3,6 +3,8 @@ echo "Iniciando upload dos arquivos"
 git add .
 echo "Aguarde [...]" ; sleep 1;
 git status
-git commit -m Atualização
+echo "Qual o motivo da atualização?"
+read motivo
+git commit -m $motivo
 echo "Aguarde[...]"
 git push origin master
